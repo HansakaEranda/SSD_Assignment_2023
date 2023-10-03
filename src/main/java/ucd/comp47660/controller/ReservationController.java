6583583e81378cdeda97d1842e1f0415ce77de41
Hansaka Eranda <87437151+HansakaEranda@users.noreply.github.com>
2023-10-03 22:49:59 +0530

    Fix Vulnerabilities in ReservationController.java

--- src/main/java/ucd/comp47660/controller/ReservationController.java
+++ src/main/java/ucd/comp47660/controller/ReservationController.java
@@ -270,11 +270,13 @@ public class ReservationController {
 //            LOGGER.info("%s", "List all users called by <" + sessionUser.getUsername() + "> with the role of <" + sessionUser.getRoles() + ">");
         }
 
+        // Updated code
         StringBuilder userRoles = new StringBuilder();
-        for (Role role : userRepository.findByUsername(sessionUser.getUsername()).getRoles()) {
-            userRoles.append(role.getName());
+        if (sessionUser != null) { // Check if sessionUser is not null
+            for (Role role : sessionUser.getRoles()) {
+                userRoles.append(role.getName());
+            }
         }
-
         user = sessionUser;
         model.addAttribute("user", user);
 
